7bcb177a250f096f765faf2ca8374fd3e00c4268
Patricia Barcena <pbarcena27@gmail.com>
2019-01-21 21:35:50 +0100

    Ejercicios dia 1

---
+++ Dia1/app.js
@@ -0,0 +1,41 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+
+console.log("Probando, probando, 1, 2, 3!");
+//1
+/*
+let nombre = "Patricia";
+let edad = 24;
+
+alert("Mi nombre es " + nombre + " y tengo " + edad + " años.");
+console.log("Mi nombre es " + nombre);
+console.log(`Mi nombre es ${nombre}`);
+*/
+
+//2
+/*
+const COLOR_ORANGE = "#FF7F00";
+*/
+
+//3
+/*
+let admin;
+let name = "John";
+
+
+admin = name;
+alert(admin);
+*/
+
+//4
+/*
+ourPlanetName = "Earth";
+currentUserName = "Hi";
+*/
+
+//5
+/*
+Only for birthday, because it won't change over time, while age will.
+*/
