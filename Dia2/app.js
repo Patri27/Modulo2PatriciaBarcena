a0c3c5add7c623b513ceadbb6a6833b34d04593e
Patricia Barcena <pbarcena27@gmail.com>
2019-01-23 18:18:29 +0100

    try

---
+++ Dia2/app.js
@@ -0,0 +1,113 @@
+/*
+ * Aquí podéis hacer los ejercicios ya
+ * practicar Javascript!
+ */
+
+console.log("Probando, probando, 1, 2, 3!");
+
+//1
+/*
+1. hello 1
+2. hello name
+3. hello Ilya
+*/
+
+//2
+/*
+"" + 1 + 0 = "10"
+"" - 1 + 0 = -1
+true + false = 1
+6 / "3" = 2
+"2" * "3" = 6
+4 + 5 + "px" = "9px"
+"$" + 4 + 5 = "$45"
+"4" - 2 = 2
+"4px" - 2 = NaN
+7 / 0 = infinity
+"  -9  " + 5 = " -9 5"
+"  -9  " - 5 = -14
+null + 1 = 1
+undefined + 1 = NaN
+*/
+
+//3
+/*
+a = 2;
+b = 2;
+c = 2;
+d = 1;
+*/
+
+//4
+/*
+a = 4
+x = 5
+*/
+
+//5
+/*
+5 > 4 = true
+"apple" > "pineapple" = false
+"2" > "12" = true
+undefined == null = true
+undefined === null = false
+null == "\n0\n" = false
+null === +"\n0\n" = false
+10 == "10" = true
+10 === "10" = false
+*/
+
+//6
+/*
+let name = prompt("What's your name?");
+alert(name);
+*/
+
+//7
+/*
+Yes, "0" is a string, therefore it returns true.
+*/
+
+//8
+/*
+let officialName = prompt("What's the 'Official' name of Javascript?");
+
+if (officialName == "ECMAScript") {
+  alert("Right!");
+} else {
+  alert("Didn’t know? ECMAScript!");
+}
+*/
+
+//9
+/*
+let number = prompt("Give me a number.");
+
+if (number > 0) {
+  alert(1);
+} else if (number < 0) {
+  alert(-1);
+} else {
+  alert(0);
+}
+*/
+
+//10
+/*
+let a = +prompt("Give me a number.");
+let b = +prompt("Give me another number.")
+(a + b < 4)? alert("Below") : alert("Over");
+*/
+
+//11
+/*
+let login = prompt("Login");
+let message =
+  login == "Employee"
+    ? alert("Hello")
+    : login == "Director"
+    ? alert("Greetings")
+    : login == ""
+    ? alert("No login")
+    : "";
+*/
